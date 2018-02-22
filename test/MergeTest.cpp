92433459aba4f3181951e43a8d62a3e553fae661
Christian Huitema <huitema@huitema.net>
2018-02-21 16:54:38 -0800

    Reduce the execution time of the Merge List test.

--- test/MergeTest.cpp
+++ test/MergeTest.cpp
@@ -87,70 +87,66 @@ bool MergeListTest::DoTest()
     FILE * F;
     char const * list[2] = { summary1, summary2 };
     int nb_files = 0;
+    unsigned int multiplier = 257;
+    CaptureSummary cs;
 
-    for (unsigned int run = 0; ret && run < 10; run++)
-    {
-        unsigned int multiplier = (1 << run);
-        CaptureSummary cs;
-
-        /* create the capture list */
+    /* create the capture list */
 #ifdef _WINDOWS
-        errno_t err = fopen_s(&F, capture_list_name, "w");
-        ret = (err == 0 && F != NULL);
+    errno_t err = fopen_s(&F, capture_list_name, "w");
+    ret = (err == 0 && F != NULL);
 #else
-        F = fopen(capture_list_name, "w");
-        ret = (F != NULL);
+    F = fopen(capture_list_name, "w");
+    ret = (F != NULL);
 #endif
 
-        if (ret)
+    if (ret)
+    {
+        for (size_t j = 0; j < multiplier; j++)
         {
-            for (size_t j = 0; j < multiplier; j++)
+            for (size_t i = 0; i < 2; i++)
             {
-                for (size_t i = 0; i < 2; i++)
+                ret = fputs(list[i], F) != EOF;
+                if (ret)
                 {
-                    ret = fputs(list[i], F) != EOF;
-                    if (ret)
-                    {
-                        ret = fputs("\n", F) != EOF;
-                    }
-                    nb_files++;
-                    if (!ret)
-                    {
-                        TEST_LOG("Cannot write file name %d on capture file\n", nb_files);
-                    }
+                    ret = fputs("\n", F) != EOF;
                 }
-            }
-            fclose(F);
-        }
-        else {
-            TEST_LOG("Cannot create capture file for run %d\n", run);
-        }
-
-        if (ret)
-        {
-            ret = cs.Merge(capture_list_name);
-            if (!ret) {
-                TEST_LOG("Merge of %d capture files failed.\n", nb_files);
-            }
-        }
-
-        if (ret)
-        {
-            CaptureSummary tcs;
-
-            ret = tcs.Load(target);
-
-            if (ret)
-            {
-                tcs.MultiplyByConstantForTest(multiplier);
-                tcs.Sort();
-
-                ret = cs.Compare(&tcs);
-
+                nb_files++;
                 if (!ret)
                 {
-                    TEST_LOG("Merge of %d capture files does not match prediction.\n", nb_files);
+                    TEST_LOG("Cannot write file name %d on capture file\n", nb_files);
                 }
+            }
+        }
+        fclose(F);
+    }
+    else {
+        TEST_LOG("Cannot create capture file \n");
+    }
+
+    if (ret)
+    {
+        ret = cs.Merge(capture_list_name);
+        if (!ret) {
+            TEST_LOG("Merge of %d capture files failed.\n", nb_files);
+        }
+    }
+
+    if (ret)
+    {
+        CaptureSummary tcs;
+
+        ret = tcs.Load(target);
+
+        if (ret)
+        {
+            tcs.MultiplyByConstantForTest(multiplier);
+            tcs.Sort();
+
+            ret = cs.Compare(&tcs);
+
+            if (!ret)
+            {
+                TEST_LOG("Merge of %d capture files does not match prediction.\n", nb_files);
             }
         }
     }
