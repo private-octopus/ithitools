abc2a604c2c26f73a6c417a663652e24c062b9a0
Christian Huitema <huitema@huitema.net>
2018-03-03 22:27:14 -0800

    Make sure the cron job writes to /var/www/html, and provides nice messages.

--- src/ithicron.sh
+++ src/ithicron.sh
@@ -12,24 +12,27 @@ LAST_LAST_DAY=$(date -d"$(date +%Y-%m-01) -1 days" +%Y-%m-%d)
 echo "Last day of previous month: $LAST_LAST_DAY"
 
 find /home/rarends/data/$DATE* | grep ".csv" > m3_this_month.txt
+echo "Found $(wc -l m3_this_month.txt) files in /home/rarends/data/$DATE*"
 M3F1=/home/ubuntu/ithi/input/M3/M3-$LAST_DAY-summary.csv
 echo "Creating summary file in $M3F1"
 ./ithitools/ithitools -S m3_this_month.txt -o $M3F1
 
-
 find /home/rarends/data/$PREVIOUS_DATE* | grep ".csv" > m3_previous_month.txt
+echo "Found $(wc -l m3_previous_month.txt) files in /home/rarends/data/$PREVIOUS_DATE*"
 M3F2=/home/ubuntu/ithi/input/M3/M3-$LAST_LAST_DAY-summary.csv
 echo "Creating summary file in $M3F2"
 ./ithitools/ithitools -S m3_previous_month.txt -o $M3F2
 
-rm m46_this_month.txt
+>m46_this_month.txt
 find /home/matiasf/* | grep $DATE | grep ".csv" >> m46_this_month.txt
+echo "Found $(wc -l m46_this_month.txt) recursive resolver reports for $DATE*"
 M46F1=/home/ubuntu/ithi/input/M46/M46-$LAST_DAY-summary.csv
 echo "Creating summary file in $M46F1"
 ./ithitools/ithitools -S m46_this_month.txt -o $M46F1
 
-rm m46_previous_month.txt
+>m46_previous_month.txt
 find /home/matiasf/* | grep $PREVIOUS_DATE | grep ".csv" >> m46_previous_month.txt
+echo "Found $(wc -l m46_previous_month.txt) recursive resolver reports for $PREVIOUS_DATE*"
 M46F2=/home/ubuntu/ithi/input/M46/M46-$LAST_LAST_DAY-summary.csv
 echo "Creating summary file in $M46F2"
 ./ithitools/ithitools -S m46_previous_month.txt -o $M46F2
@@ -44,4 +47,4 @@ echo "Computing metrics for $LAST_DAY"
 ./ithitools/ithitools -i /home/ubuntu/ithi -d $LAST_DAY -m
 
 echo "Computing JSON Data for publication"
-./ithitools/ithitools -i /home/ubuntu/ithi -p
+./ithitools/ithitools -i /home/ubuntu/ithi -w /var/www/html/ -p
