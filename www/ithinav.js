6e1e3410a6139ce49b2bae85264a99287bb8c18f
Christian Huitema <huitema@huitema.net>
2018-03-26 17:35:48 -0700

    Adding placeholders for M1 and M5.

--- www/ithinav.js
+++ www/ithinav.js
@@ -5,11 +5,11 @@ document.getElementById("navMenu").innerHTML =
   '<div class="dropdown">' +
     '<a href="index.html">Metrics</a>' +
     '<div class="dropdown-content">' +
-      '<a href="index.html">M1: Whois</a>' +
+      '<a href="graph-m1.html">M1: Whois</a>' +
       '<a href="graph-m2.html">M2: Abuses</a>' +
       '<a href="graph-m3.html">M3: Root Servers</a>' +
       '<a href="graph-m4.html">M4: Usage</a>' +
-      '<a href="index.html">M5: Accuracy</a>' +
+      '<a href="graph-m5.html">M5: Accuracy</a>' +
       '<a href="graph-m6.html">M6: IANA</a>' +
       '<a href="graph-m7.html">M7: DNSSEC</a>' +
     '</div>' +
