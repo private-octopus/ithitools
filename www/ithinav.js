08402c3e437378453c2a78a4e8766f54c8b83392
Christian Huitema <huitema@huitema.net>
2018-04-02 16:19:53 -0700

    Check missed texts in menu and align data for M1.

--- www/ithinav.js
+++ www/ithinav.js
@@ -8,8 +8,8 @@ document.getElementById("navMenu").innerHTML =
       '<a href="graph-m1.html">M1: Whois</a>' +
       '<a href="graph-m2.html">M2: Abuses</a>' +
       '<a href="graph-m3.html">M3: Root Servers</a>' +
-      '<a href="graph-m4.html">M4: Usage</a>' +
-      '<a href="graph-m5.html">M5: Accuracy</a>' +
+      '<a href="graph-m4.html">M4: Recursive Servers</a>' +
+      '<a href="graph-m5.html">M5: TBD</a>' +
       '<a href="graph-m6.html">M6: IANA</a>' +
       '<a href="graph-m7.html">M7: DNSSEC</a>' +
     '</div>' +
