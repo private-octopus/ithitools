1327297326aa5e7fbfdb26a0136746d1b77b3af3
Christian Huitema <huitema@huitema.net>
2018-05-24 17:53:15 -0700

    Fixes to control file

--- debian/manpage.1.ex
+++ debian/manpage.1.ex
@@ -1,56 +1,77 @@
-.\"                                      Hey, EMACS: -*- nroff -*-
 .\" (C) Copyright 2018 Christian Huitema <huitema@huitema.net>,
-.\"
-.\" First parameter, NAME, should be all caps
-.\" Second parameter, SECTION, should be 1-8, maybe w/ subsection
-.\" other parameters are allowed: see man(7), man(1)
-.TH Ithitools SECTION "May 23 2018"
+.TH Ithitools 7 "May 23 2018"
 .\" Please adjust this date whenever revising the manpage.
-.\"
-.\" Some roff macros, for reference:
-.\" .nh        disable hyphenation
-.\" .hy        enable hyphenation
-.\" .ad l      left justify
-.\" .ad b      justify to both left and right margins
-.\" .nf        disable filling
-.\" .fi        enable filling
-.\" .br        insert line break
-.\" .sp <n>    insert n+1 empty lines
-.\" for manpage-specific macros, see man(7)
 .SH NAME
-ithitools \- program to do something
+ITHITOOLS -- a tool for ITHI data extraction and metric computation.
 .SH SYNOPSIS
 .B ithitools
-.RI [ options ] " files" ...
+.RI [ options ] -c " files.PCAP " ...
 .br
-.B bar
-.RI [ options ] " files" ...
+.B ithitools
+.RI [ options ] -s " files.csv" ...
 .SH DESCRIPTION
 This manual page documents briefly the
 .B ithitools
-and
-.B bar
-commands.
+command. TODO: Should also document the libithicap.so plug-in.
 .PP
-.\" TeX users may be more comfortable with the \fB<whatever>\fP and
-.\" \fI<whatever>\fP escape sequences to invode bold face and italics,
-.\" respectively.
-\fBithitools\fP is a program that...
 .SH OPTIONS
-These programs follow the usual GNU command line syntax, with long
-options starting with two dashes (`-').
+These programs follow the simplified command line syntax, with
+single character options starting with one dashes (`-').
 A summary of options is included below.
 For a complete description, see the Info files.
 .TP
-.B \-h, \-\-help
-Show summary of options.
+.B \-? \-h
+Show summary of options
 .TP
-.B \-v, \-\-version
-Show version of program.
+.B \-c
+process DNS traffic capture files in PCAP format,
+PCAP files listed the input files arguments.
+.TP
+.B \-s
+process summary files, from previous captures.
+CSV files listed the input files arguments,
+or in a text file, see -S argument description.
+.TP
+Options used in capture mode:
+.TP
+.B \-o file.csv
+output file containing the computed summary.
+.TP
+.B \-R root-addr.txt
+text file containing the list of root server addresses.
+.TP
+.B \-a res-addr.txt
+allowed list of resolver addresses. Traffic to or from
+addresses in this list will not be filtered out by the
+excessive traffic filtering mechanism.
+.TP
+.B \-x res-addr.txt
+excluded list of resolver addresses. Traffic to or from
+these addresses will be ignored when extracting traffic.
+.TP
+.B \-f
+Filter out address sources that generate too much traffic.
+.TP
+.B \-T
+Capture a list of TLD found in user queries.
+.TP
+.B \-t tld-file.txt
+Text file containing a list of registered TLD, one per line.
+.TP
+.B \-u tld-file.txt
+Text file containing special usage TLD (RFC6761).
+.TP
+.B \-n number
+Number of strings in the list of leaking domains(M4).
+.TP
+Options used in summary mode:
+.TP
+.B \-o file.csv
+output file containing the computed summary.
+.TP
+.B \-S filelist.txt
+process summary files listed in file list.
 .SH SEE ALSO
-.BR bar (1),
-.BR baz (1).
+.BR dnscap (1)
 .br
-The programs are documented fully by
-.IR "The Rise and Fall of a Fooish Bar" ,
-available via the Info system.
+
