900e8cd17e29b0f9611e6325b7dc42f4234fe730
Christian Huitema <huitema@huitema.net>
2018-06-14 16:45:03 -0700

    Cleaning the control files for the deb packages.

--- debian/ithitools.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the ithitools package
-#
-0 4	* * *	root	[ -x /usr/bin/ithitools_maintenance ] && /usr/bin/ithitools_maintenance
--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(ithitools):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="ithitools" command="/usr/bin/ithitools"
