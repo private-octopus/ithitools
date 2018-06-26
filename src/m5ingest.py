bde5994f9d1eb66b1921e5b27880d31ceb4a40f7
Ubuntu <ubuntu@ip-172-31-48-45.ec2.internal>
2018-06-26 02:45:15 +0000

    Make sure that the M5 averages are computed using floating point

--- src/m5ingest.py
+++ src/m5ingest.py
@@ -8,7 +8,8 @@ from os import walk
 class m5_summary:
     def __init__(self):
         self.i_count = 0
-        self.i_total = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.total = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 
+                      0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
         self.key_list = ["M5.1.1", "M5.1.2", "M5.1.3", "M5.1.4", "M5.1.5", "M5.1.6",
            "M5.2.1", "M5.2.2", "M5.2.3", "M5.3.1", "M5.3.2", "M5.4.1",
            "M5.4.2","M5.5"]
@@ -25,7 +26,7 @@ class m5_summary:
             m5_data = json.loads(m5_text)
             i = 0
             while i < len(self.key_list) :
-                self.i_total[i] += m5_data[0][self.key_list[i]]
+                self.total[i] += m5_data[0][self.key_list[i]]
                 i += 1
             m5_file.close()
     
@@ -34,7 +35,7 @@ class m5_summary:
         if (self.i_count > 1) :
             i = 0
             while i < len(self.key_list) :
-                self.i_total[i] /= self.i_count;
+                self.total[i] /= self.i_count;
                 i += 1
             self.i_count = 1
     
@@ -46,7 +47,7 @@ class m5_summary:
             entry = "";
             if (self.sub_entry[i] != 0) :
                 entry = str(self.sub_entry[i]);
-            csv_file.write(self.sub_met[i] + ", " + entry + ", " + str(self.i_total[i]) + ",\n")
+            csv_file.write(self.sub_met[i] + ", " + entry + ", " + str(self.total[i]) + ",\n")
             i += 1
         csv_file.close()
 
@@ -55,16 +56,14 @@ class m5_summary:
 summary = m5_summary()
 mypath = sys.argv[1]
 for (dirpath, dirnames, filenames) in walk(mypath):
-    print("Found dir:" + dirpath);
     for file_name in filenames :
         if (file_name.startswith("m5-")):
             file_path = os.path.join(dirpath, file_name)
-            print("Found " + file_path)
             summary.add_to_summary(file_path)
 print("Found " + str(summary.i_count) + " summaries.")
 summary.average()
 i = 0
 while i < len(summary.key_list) :
-    print("m5[" + summary.key_list[i] + "] = " + str(summary.i_total[i]))
+    print("m5[" + summary.key_list[i] + "] = " + str(summary.total[i]))
     i += 1
 summary.save_as_csv(sys.argv[2])
