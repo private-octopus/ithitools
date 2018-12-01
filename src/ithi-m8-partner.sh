68557ade56d04d801740d1e316cceec9c4132916
Ubuntu <ubuntu@ip-172-31-48-45.ec2.internal>
2018-12-01 05:51:41 +0000

    Adding M8 script and publishing authoritative data

--- src/ithi-m8-partner.sh
+++ src/ithi-m8-partner.sh
@@ -36,19 +36,19 @@ echo "Last day of this month: $LAST_DAY"
 LAST_LAST_DAY=$(date -d "$(date -d $DATE_CURRENT +%Y-%m-01) -1 days" +%Y-%m-%d)
 echo "Last day of previous month: $LAST_LAST_DAY"
 
->m46_$1_this_month.txt
-grep $1 < m46_this_month.txt >> m46_$1_this_month.txt
-echo "Found $(wc -l m46_$1_this_month.txt) recursive resolver reports for $1 at rm $DATE*"
-M46F1=ithi/$1/input/M46/M46-$LAST_DAY-summary.csv
-echo "Creating summary file in $M46F1"
-./ithitools/ithitools -S m46_$1_this_month.txt -o $M46F1
+>m8_$1_this_month.txt
+grep $1 < m8_this_month.txt >> m8_$1_this_month.txt
+echo "Found $(wc -l m8_$1_this_month.txt) authoritative resolver reports for $1 at rm $DATE*"
+M8F1=ithi/$1/input/M8/M8-$LAST_DAY-summary.csv
+echo "Creating summary file in $M8F1"
+./ithitools/ithitools -S m8_$1_this_month.txt -o $M8F1
 
->m46_$1_previous_month.txt
-grep $1 < m46_previous_month.txt >> m46_$1_previous_month.txt
-echo "Found $(wc -l m46_$1_previous_month.txt) recursive resolver reports for $1 at $PREVIOUS_DATE*"
-M46F2=ithi/$1/input/M46/M46-$LAST_LAST_DAY-summary.csv
-echo "Creating summary file in $M46F2"
-./ithitools/ithitools -S m46_$1_previous_month.txt -o $M46F2
+>m8_$1_previous_month.txt
+grep $1 < m8_previous_month.txt >> m8_$1_previous_month.txt
+echo "Found $(wc -l m8_$1_previous_month.txt) authoritative resolver reports for $1 at $PREVIOUS_DATE*"
+M8F2=ithi/$1/input/M8/M8-$LAST_LAST_DAY-summary.csv
+echo "Creating summary file in $M8F2"
+./ithitools/ithitools -S m8_$1_previous_month.txt -o $M8F2
 
 echo "Computing $1 metrics for $LAST_LAST_DAY"
 ./ithitools/ithitools -i ithi/$1 -d $LAST_LAST_DAY -m
