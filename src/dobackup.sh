2c18026174eb45e9a4d70f0f9cbd76fcbc589784
Ubuntu <ubuntu@ip-172-31-48-45.ec2.internal>
2020-05-20 04:15:10 +0000

    Archive backup script

---
+++ src/dobackup.sh
@@ -0,0 +1,10 @@
+tar -zcf /home/backupper/backup/tar-ithi.gz /home/ubuntu/ithi/
+tar -zcf /home/backupper/backup/tar-gih.gz /home/gih/data/
+tar -zcf /home/backupper/backup/tar-kaznic.gz /home/kaznic/data/
+tar -zcf /home/backupper/backup/tar-unlp.gz /home/matiasf/*.csv
+tar -zcf /home/backupper/backup/tar-nawala.gz /home/nawala/data/
+tar -zcf /home/backupper/backup/tar-rarends.gz /home/rarends/data/
+tar -zcf /home/backupper/backup/tar-twnic.gz /home/twnic/data/
+tar -zcf /home/backupper/backup/tar-uccgh.gz /home/uccgh/data/
+tar -zcf /home/backupper/backup/tar-viktor.gz /home/viktor/data/
+
