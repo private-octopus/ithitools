020989ed644cd29c390eee4da33f1f610e2a67db
Ubuntu <ubuntu@ip-172-31-48-45.ec2.internal>
2020-05-20 04:02:48 +0000

    Debug ithicatchup.sh

--- src/ithicatchup.sh
+++ src/ithicatchup.sh
@@ -3,22 +3,21 @@ pwd
 cd /home/ubuntu
 pwd
 DATE_CURRENT=$1
+echo "Current: $DATE_CURRENT"
 DATE=$(date -d $DATE_CURRENT +%Y%m)
-PREVIOUS_DATE=$(date -d $DATE_PREVIOUS +%Y%m)
 YEAR=$(date -d $DATE_CURRENT +%Y)
 MM=$(date -d $DATE_CURRENT +%m)
-DATE_DASH=$(date -d $DATE_CURRENT +%Y-
+DATE_DASH=$(date -d $DATE_CURRENT +%Y-%m-%d)
 FIRST_DAY=$(date -d $YEAR-$MM-01 +%Y-%m-%d)
 DAY_AFTER_MONTH=$(date -d "$FIRST_DAY +1 months" +%Y-%m-01)
 LAST_DAY=$(date -d "$DAY_AFTER_MONTH -1 day" +%Y-%m-%d)
-echo "Current: $DATE_CURRENT"
 echo "First day: $FIRST_DAY"
 echo "Day after month: $DAY_AFTER_MONTH"
 echo "This month selector: $DATE (or $DATE_DASH), Year: $YEAR, Month: $MM"
 echo "Last day of this month: $LAST_DAY"
 
-find /home/rarends/data/$DATE* | grep ".csv" > 3_catch_month.txt
-echo "Found $(wc -l 3_catch_month.txt) files in /home/rarends/data/$DATE*"
+find /home/rarends/data/$DATE* | grep ".csv" > m3_catch_month.txt
+echo "Found $(wc -l m3_catch_month.txt) files in /home/rarends/data/$DATE*"
 M3F1=/home/ubuntu/ithi/input/M3/M3-$LAST_DAY-summary.csv
 echo "Creating summary file in $M3F1"
 ./ithitools/ithitools -S m3_catch_month.txt -o $M3F1
@@ -51,9 +50,6 @@ echo "Computing metrics for $LAST_DAY"
 
 echo "Ingesting M5 for $LAST_DAY"
 python ithitools/src/m5ingest.py /home/gih/data/$YEAR/$MM/ /home/ubuntu/ithi/M5/M5-$LAST_DAY.csv $LAST_DAY
-
-echo "Computing JSON Data for publication"
-./ithitools/ithitools -i /home/ubuntu/ithi -w /var/www/html -p
 
 
 
