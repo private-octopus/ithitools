a69837c0707618071b3f16fe97471f0243288579
Christian Huitema <huitema@huitema.net>
2020-09-02 21:08:59 -0700

    Handle AS and Frequent number

--- stats/address_file.py
+++ stats/address_file.py
@@ -46,8 +46,10 @@ class address_file_line:
         self.ip = ip
         self.asn = 0
         self.frequent = 0
+        self.dga = 0
         self.nx_domain = 0
         self.arpa = 0
+        self.com = 0
         self.tld = 0
         self.tld_min_delay = -1
         self.nb_files = 0
@@ -55,11 +57,17 @@ class address_file_line:
 
     def update(self, aline):
         if aline.nx_domain:
-            self.nx_domain += aline.count
+            if aline.name_type == "dga":
+                self.dga += aline.count
+            else:
+                self.nx_domain += aline.count
         elif aline.tld.upper() == "ARPA":
             self.arpa += aline.count
         else:
-            self.tld += aline.count
+            if aline.tld.upper() == "COM":
+                self.com += aline.count
+            else:
+                self.tld += aline.count
             if aline.tld_min_delay > 0 and (self.tld_min_delay < 0 or self.tld_min_delay > aline.tld_min_delay):
                 self.tld_min_delay = aline.tld_min_delay
 
@@ -69,12 +77,13 @@ class address_file_line:
             self.last_file = last_file
 
     def csv_head():
-        return "ip,asn,frq,total,nx_domain,arpa,tld,tld_min_delay,nb_files\n"
+        return "ip,asn,frq,total,dga,nx_domain,arpa,com,tld,tld_min_delay,nb_files\n"
 
     def to_csv(self):
-        total = self.nx_domain + self.arpa + self.tld
-        s = self.ip + "," + str(self.asn) + "," + str(self.frequent) + "," + str(total) + "," + \
-            str(self.nx_domain) + "," + str(self.arpa) + "," + str(self.tld) + "," + \
+        total = self.dga + self.nx_domain + self.arpa + self.com + self.tld
+        s = str(self.ip) + "," + str(self.asn) + "," + str(self.frequent) + "," + str(total) + "," + \
+            str(self.dga) + "," + str(self.nx_domain) + "," + \
+            str(self.arpa) + "," + str(self.com) + "," + str(self.tld) + "," + \
             str(self.tld_min_delay) + "," + str(self.nb_files) + "\n"
         return s
 
@@ -84,11 +93,13 @@ class address_file_line:
             self.ip = csv[0].strip()
             self.asn = int(csv[1].strip())
             self.frequent = int(csv[2].strip())
-            self.nx_domain = int(csv[4].strip())
-            self.arpa = int(csv[5].strip())
-            self.tld = int(csv[6].strip())
-            self.tld_min_delay = int(csv[7].strip())
-            self.nb_files = int(csv[8].strip())
+            self.dga = int(csv[4].strip())
+            self.nx_domain = int(csv[5].strip())
+            self.arpa = int(csv[6].strip())
+            self.com = int(csv[7].strip())
+            self.tld = int(csv[8].strip())
+            self.tld_min_delay = int(csv[9].strip())
+            self.nb_files = int(csv[10].strip())
         except:
             if csv[0] != "ip":
                 traceback.print_exc()
@@ -96,8 +107,10 @@ class address_file_line:
             self.ip = ""
     
     def add(self, other):
+        self.dga += other.dga
         self.nx_domain += other.nx_domain
         self.arpa += other.arpa
+        self.com += other.com
         self.tld += other.tld
         if other.tld_min_delay > 0 and (self.tld_min_delay < 0 or self.tld_min_delay > other.tld_min_delay):
             self.tld_min_delay = other.tld_min_delay
--- stats/count_ip.py
+++ stats/count_ip.py
@@ -5,6 +5,19 @@
 # This script takes as input a list of "address" files and provides a summary
 # with one line per address in the file. The it computes a simple summary 
 # with one line per address.
+#
+# TODO: deal with clusters. The recording of the cluster nodes should be logically
+# merged, so that a given address is not counted twice if present on two cluster
+# nodes. This means sorting by <location><time><cluster-id>, and spreading all
+# files for the same time and location to the same parallel processor. It also
+# means counting "minute slices" instead of files.
+#
+# TODO: Add AS number.
+#
+# TODO: Integrate frequent IP documentation. There are by definition 10,000 IP
+# addresses in the frequent IP list, which means that it makes more sense to just
+# loop on the list and check the list addresses one by one, rather than
+# starting with the collected IP dict and checking every address.
 
 import codecs
 import sys
@@ -16,6 +29,8 @@ import gzip
 import concurrent.futures
 import traceback
 import time
+import ipaddress
+import ip2as
 
 
 class file_bucket:
@@ -27,6 +42,7 @@ class file_bucket:
         self.previous_ip = ""
         self.bucket_id = 0
         self.file_path = ""
+        self.ip_short = "0.0.0.0"
 
     def add_line(self, line, input_file):
         aline = address_line()
@@ -34,10 +50,13 @@ class file_bucket:
         if len(aline.ip) > 0:
             if aline.ip != self.previous_ip:
                 self.previous_ip = aline.ip
-                if not aline.ip in self.ip_dict:
-                    self.ip_dict[aline.ip] = address_file_line(aline.ip)
-                self.ip_dict[aline.ip].add_file(input_file)
-            self.ip_dict[aline.ip].update(aline)
+                ip_binary = ipaddress.ip_address(aline.ip)
+                self.ip_short = ip_binary.compressed
+                aline.ip = self.ip_short
+                if not self.ip_short in self.ip_dict:
+                    self.ip_dict[self.ip_short] = address_file_line(aline.ip)
+                self.ip_dict[self.ip_short].add_file(input_file)
+            self.ip_dict[self.ip_short].update(aline)
             self.total_count += aline.count
 
     def load(self):
@@ -72,13 +91,16 @@ def load_bucket(bucket):
 # Main loop
 
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: " + sys.argv[0] + " <input-folder> <count_file.csv> <temp_prefix>\n")
+    if len(sys.argv) != 7:
+        print("Usage: " + sys.argv[0] + "<count_file.csv> <temp_prefix> <frequent-ip.csv> <ip2as.csv> <ip2asv6.csv> <input-folder> \n")
         exit(1)
 
-    input_path = sys.argv[1]
-    count_file = sys.argv[2]
-    temp_prefix = sys.argv[3]
+    count_file = sys.argv[1]
+    temp_prefix = sys.argv[2]
+    frequent_ip = sys.argv[3]
+    ip2as_in = sys.argv[4]
+    ip2asv6_in = sys.argv[5]
+    input_path = sys.argv[6]
 
     input_files = [f for f in listdir(input_path ) if isfile(join(input_path, f))]
 
@@ -130,7 +152,7 @@ def main():
             for line in open(bucket.file_path):
                 file_line = address_file_line("")
                 file_line.from_csv(line)
-                if len(file_line.ip) > 0:
+                if file_line.ip != "":
                     if file_line.ip in ip_dict:
                         # merge the two lines
                         ip_dict[file_line.ip].add(file_line)
@@ -141,17 +163,49 @@ def main():
             traceback.print_exc()
             print("Abandon bucket " + str(bucket.bucket_id))
         print("After bucket %d, %d IP, %d transactions"%(bucket.bucket_id, len(ip_dict), total_count))
-    end_time = time.time()
-    print("Complete in " + str(end_time - start_time))
+    summary_time = time.time()
     print("Threads took " + str(bucket_time - start_time))
-    print("Summary took " + str(end_time - bucket_time))
+    print("Summary took " + str(summary_time - bucket_time))
+
+    # document which files are "frequent"
+    for line in open(frequent_ip):
+        parts = line.split(",")
+        try:
+            ip_address = ipaddress.ip_address(parts[0].strip())
+            ip_text = str(ip_address)
+            if ip_text in ip_dict:
+                ip_dict[ip_text].frequent = 1
+        except:
+            pass
+    
+    frequent_time = time.time()
+    print("Frequent IP took " + str(frequent_time - summary_time))
+
+    # document the AS number
+    ipv4table = ip2as.ip2as_table()
+    ipv4table.load(ip2as_in)
+    ipv6table = ip2as.ip2as_table()
+    ipv6table.load(ip2asv6_in)
+
+    for ip in ip_dict:
+        if ":" in ip:
+            ip_dict[ip].asn = ipv6table.get_asn(ip)
+        else:
+            ip_dict[ip].asn = ipv4table.get_asn(ip)
+
+    as_time = time.time()
+    print("AS lookup took " + str(as_time - frequent_time))
+
 
     f_out = open(count_file, "wt")
     f_out.write(address_file_line.csv_head())
     for ip_address in ip_dict:
         f_out.write(ip_dict[ip_address].to_csv())
     f_out.close();
+    
+    end_time = time.time()
     print("Processed " + str(len(ip_dict)) + " IP addresses, " + str(total_count) + " transactions.")
+    print("Complete in " + str(end_time - start_time))
 
 if __name__ == '__main__':
     #freeze_support()
