757a201f07699f180e1566e37cd252533943f3c4
Ubuntu <ubuntu@ip-172-31-48-45.ec2.internal>
2021-05-21 01:22:19 +0000

    Update oneMonth M5 M8 scripts.

--- src/oneMonthM8.sh
+++ src/oneMonthM8.sh
@@ -5,7 +5,7 @@ pwd
 YEAR=$1
 MM=$2
 FIRST_DAY=$(date -d $YEAR-$MM-01 +%Y-%m-%d)
-DATE=$(date -d $FIRST_DAY +%Y%m)
+DATE=$(date -d $FIRST_DAY +%Y-%m)
 DAY_AFTER_MONTH=$(date -d "$FIRST_DAY +1 months" +%Y-%m-01)
 LAST_DAY=$(date -d "$DAY_AFTER_MONTH -1 day" +%Y-%m-%d)
 echo "First day: $FIRST_DAY"
@@ -14,8 +14,8 @@ echo "This month selector: $DATE"
 echo "Last day of this month: $LAST_DAY"
 
 >m8_this_month.txt
-find /home/kaznic/* | grep $DATE_DASH | grep ".csv" >> m8_this_month.txt
-find /home/twnic/data/* | grep $DATE_DASH | grep ".csv" >> m8_this_month.txt
+find /home/kaznic/* | grep $DATE | grep ".csv" >> m8_this_month.txt
+find /home/twnic/data/* | grep $DATE | grep ".csv" >> m8_this_month.txt
 echo "Found $(wc -l m8_this_month.txt) authoritative resolver reports for $DATE_DASH*"
 M8F1=/home/ubuntu/ithi/input/M8/M8-$LAST_DAY-summary.csv
 echo "Creating summary file in $M8F1"
--- src/oneMonthM8Partner.sh
+++ src/oneMonthM8Partner.sh
@@ -1,11 +1,10 @@
 LAST_DAY=$2
 
->m8_$1_this_month.txt
-grep $1 < m8_this_month.txt >> m8_$1_this_month.txt
+grep $1 < m8_this_month.txt > m8_$1_one_month.txt
 echo "Found $(wc -l m8_$1_one_month.txt) authoritative resolver reports for $1 at $2"
-M8F1=ithi/$1/input/M46/M46-$LAST_DAY-summary.csv
+M8F1=ithi/$1/input/M8/M8-$LAST_DAY-summary.csv
 echo "Creating summary file in $M8F1"
-./ithitools/ithitools -S m8_$1_this_month.txt -o $M8F1
+./ithitools/ithitools -S m8_$1_one_month.txt -o $M8F1
 
 echo "Computing metrics for $LAST_DAY"
 ./ithitools/ithitools -i ithi/$1 -d $LAST_DAY -m
