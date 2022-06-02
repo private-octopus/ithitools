4d228d74357d53ecda6be5b7eed031ad0ad35270
Christian Huitema <huitema@huitema.net>
2022-06-02 11:24:39 -0700

    fix again

--- lib/ithipublisher.cpp
+++ lib/ithipublisher.cpp
@@ -75,7 +75,7 @@ bool ithipublisher::CollectMetricFiles()
     bool ret = snprintf(dir_met_name, sizeof(dir_met_name), "%s%sM%d%s", ithi_folder, ITHI_FILE_PATH_SEP, metric_id, ITHI_FILE_PATH_SEP) > 0;
 
 #if 1
-    printf("Collecting files for metric %s\n", metric_id);
+    printf("Collecting files for metric %d\n", metric_id);
 #endif
 
     if (ret)
