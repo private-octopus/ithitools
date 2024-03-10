792362eb3b8320255e789acad8c409a7c8d495cb
Christian Huitema <huitema@huitema.net>
2024-03-10 13:34:53 -0700

    Fix sh script issues

--- src/ipstats.sh
+++ src/ipstats.sh
@@ -5,6 +5,8 @@ IPSTATS_DIR="$HOME/ipstats"
 IMRS_CBOR=/data/ITHI/cbor/IMRS/
 MONTH=202403
 SCRIPT="$HOME/ithitools/src/imrsrsv.py"
+RUN_DATE=$(date '%(%Y-%m-%d)T\n')
+RUN_LOG=`tmp/$RUN_DATE.$$.txt`
 
 echo "Home: $HOME"
 echo "ithitools: $ITHITOOLS"
@@ -12,5 +14,6 @@ echo "ipstats: $IPSTATS_DIR"
 echo "imrs: $IMRS_CBOR"
 echo "Month: $MONTH"
 echo "Script: $SCRIPT"
+echo "Log: $RUN_LOG"
 
-python3.6 $SCRIPT $IMRS_CBOR $IPSTATS_DIR $MONTH $ITHITOOLS
+python3.6 $SCRIPT $IMRS_CBOR $IPSTATS_DIR $MONTH $ITHITOOLS > $RUN_LOG
