091f9f7a46223a1aa20faac173b0444dfd4acc30
Christian Huitema <huitema@huitema.net>
2024-04-29 11:25:14 -0700

    anycast statistics

---
+++ imrs/imrs.py
@@ -0,0 +1,90 @@
+#
+# This script will try to build a sample of the input file.
+# The purpose of the sample is, get a realistic test file
+# that is small enough for iterative development, measures,
+# etc., yet big enough to obtain statistically significant
+# results.
+#
+# Usage: imrs_sample.py <input_file> <sampling rate in %> <output_file>
+#
+
+import sys
+import traceback
+import random
+import time
+import concurrent.futures
+import math
+import os
+from os import listdir
+from os.path import isfile, isdir, join
+
+
+def imrs_parse_one_number(parts, parsed):
+    v = 0
+    p = parts[parsed].strip()
+    v = int(parts[parsed])
+    parsed += 1
+    return v, parsed
+
+def imrs_parse_one_vector(parts, parsed, v):
+    for i in range(0, len(v)):
+        v[i],parsed = imrs_parse_one_number(parts, parsed)
+    return parsed
+
+class imrs_hyperloglog:
+    def __init__(self):
+        self.E = 0.0
+        self.hllv=[0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0,0.0]
+        pass;
+    def parse(self, parts, parsed):
+        self.E = float(parts[parsed].strip())
+        parsed += 1
+        for i in range(0, len(self.hllv)):
+            self.hllv[i], parsed = imrs_parse_one_number(parts,parsed)
+        return parsed
+
+class imrs_record:
+    def __init__(self):
+        self.ip = ""
+        self.query_volume = 0
+        self.hourly_volume = [ 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.daily_volume = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+        self.arpa_count = 0
+        self.no_such_domain_queries = 0
+        self.no_such_domain_reserved = 0
+        self.no_such_domain_frequent = 0
+        self.no_such_domain_chromioids = 0
+        self.tld_counts = [0,0,0,0,0,0,0,0]
+        self.tld_hyperlog = imrs_hyperloglog()
+        self.sld_counts = [0,0,0,0,0,0,0,0]
+        self.sld_hyperlog = imrs_hyperloglog()
+        self.name_parts = [0,0,0,0,0,0,0,0]
+        self.rr_types = [0,0,0,0,0,0,0,0]
+        self.locales = [0,0,0,0,0,0,0,0]
+
+    def parse_imrs(self, line):
+        ok = False
+        try:
+            parts = line.split(",")
+            self.ip = parts[0].strip()
+            parsed = 1
+            self.query_volume, parsed = imrs_parse_one_number(parts, parsed)
+            parsed = imrs_parse_one_vector(parts, parsed, self.hourly_volume)
+            parsed = imrs_parse_one_vector(parts, parsed, self.daily_volume)
+            self.arpa_count, parsed = imrs_parse_one_number(parts, parsed)
+            self.no_such_domain_queries, parsed = imrs_parse_one_number(parts, parsed)
+            self.no_such_domain_reserved, parsed = imrs_parse_one_number(parts, parsed)
+            self.no_such_domain_frequent, parsed = imrs_parse_one_number(parts, parsed)
+            self.no_such_domain_chromioids, parsed = imrs_parse_one_number(parts, parsed)
+            parsed = imrs_parse_one_vector(parts, parsed, self.tld_counts)
+            parsed = self.tld_hyperlog = imrs_hyperloglog(parts, parsed)
+            parsed = imrs_parse_one_vector(parts, parsed, self.sld_counts)
+            parsed = self.sld_hyperlog = imrs_hyperloglog(parts, parsed)
+            parsed = imrs_parse_one_vector(parts, parsed, self.name_parts)
+            parsed = imrs_parse_one_vector(parts, parsed, self.rr_types)
+            parsed = imrs_parse_one_vector(parts, parsed, self.locales)
+            ok = True
+        except Exception as e:
+            traceback.print_exc()
+            print("Cannot parse IMRS Record after " + str(parsed) + " parts:\n" + line.strip()  + "\nException: " + str(e))
+        return ok
---
+++ imrs/imrs_anycast.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+# coding=utf-8
+#
+# This script analyzes how traffic is spread by routing protocols.
+# For each IP address, we compute the fraction of traffic going to a specific
+# location (cluster), as already compute in the clusters "monthly" files.
+# Once we have computed all the data for an IP address, we make the hypothesis
+# that there is a dominant cluster, and then some "diverted" traffic
+# routed to other locations. We can then sum that data per dominant cluster:
+#   - total amount of traffic
+#   - amount routed to other places.
+# We can visualize that as a graph, showing the effects of anycast routing,
+# but we can also express that as a csv file, with N lines and N columns,
+# allowing for further visualization.
+#
+# Usage: py imrs_anycast.py <cluster_folder> <output_file>
+import sys
+import traceback
+import os
+from os import listdir
+from os.path import isfile, isdir, join
+from .imrs import imrs_record
+
+class ip_clusters:
+    def __init__(self, ip):
+        self.ip = ip
+        self.clusters=dict()
+
+class cluster_clusters:
+    def __init__(self, cluster):
+        self.cluster = cluster
+        self.clusters=dict()
+
+class cluster_item:
+    def __init__(self, cluster, volume):
+        self.cluster = cluster
+        self.volume = volume
+
+def parse_cluster(cluster_folder, cluster_file, ips, min_volume):
+    # extract cluster name from file name
+    parts = cluster_file.split(".")
+    cluster = parts[0]
+    # compose path
+    file_path = join(cluster_folder, cluster_file, ips)
+    # record the clusters in which the address is found
+    for line in open(file_path,"r"):
+        r = imrs_record()
+        if r.parse_imrs(line) and r.query_volume > min_volume:
+            if not r.ip in ips:
+                ips[r.ip] = ip_clusters[ip]
+            if not cluster in ips[r.ip].clusters:
+                ips[r.ip].clusters[cluster] = 0
+            ips[r.ip].clusters[cluster] += r.query_volume
+
+def compute_cross_path(ips, cross_path):
+    for ip in ips:
+        main_cluster = ""
+        v_max = 0
+        v_sum = 0
+        for cluster in ips[r.ip].clusters:
+            v_sum += ips[r.ip].clusters[cluster]
+            if ips[r.ip].clusters[cluster] > v_max:
+                v_max = ips[r.ip].clusters[cluster]
+                main_cluster = cluster
+        if not main_cluster in cross_path:
+            cross_path[main_cluster] = cluster_clusters[cluster]
+        for cluster in ips[r.ip].clusters:
+            if not cluster in cross_path[main_cluster].clusters:
+                cross_path[main_cluster].clusters[cluster] = 0
+            cross_path[main_cluster].clusters[cluster] += ips[r.ip].clusters[cluster]
+
+def cross_path_output(cross_path, output_file):
+    with open(output_file, "w") as F:
+        cluster_list = list(cross_path.keys()).sorted()
+        for cluster in cluster_list:
+            c_c = cross_path[cluster]
+            c_volume = c_c.clusters[cluster]
+            F.write(cluster + ",")
+            F.write(str(c_volume)+",")
+            v = []
+            for cluster2 in c_c.clusters:
+                if cluster2 != cluster:
+                    v.append(cluster_item(cluster2, c_c.clusters[cluster2]))
+                    c_volume += c_c.clusters[cluster2]
+            vs = sorted(v,key=lambda cl: cl.volume, reversed=True)
+            for vs_c in vs:
+                F.write(vs_c.cluster + ",")
+                F.write(str(vs_c.volume) + ",")
+                F.write(str(100*vs_c.volume/c_volume) + "%,")
+
+# main
+if len(sys.argv) != 4:
+    print("Usage: py imrs_anycast.py <cluster_folder> <output_file> min_volume")
+    exit(1)
+cluster_folder = sys.argv[1]
+output_file = sys.argv[2]
+try:
+    min_volume = int(sys.argv[3])
+except:
+    print("Cannot parse number of transactions from: " + sys.argv[3])
+    exit(1)
+
+ips = dict()
+clusters = listdir(cluster_folder)
+for cluster_file in clusters:
+    parse_cluster(cluster_folder, cluster_file, ips)
+compute_cross_path(ips, cross_path)
+
+
+
+
+
