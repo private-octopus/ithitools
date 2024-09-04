f4decdded9e6076bf1e456d3a4c8c4e4778e2462
Christian Huitema <christian.huitema@DS.ICANN.ORG@octo-compute2.dc.icann.org>
2024-09-04 22:16:10 +0000

    Ipstats for 202409 collection

--- src/ipstats.sh
+++ src/ipstats.sh
@@ -3,7 +3,7 @@ HOME=/home/christian.huitema@DS.ICANN.ORG
 ITHITOOLS="$HOME/ithitools/ithitools"
 IPSTATS_DIR="/data/huitema/ipstats"
 IMRS_CBOR=/data/ITHI/cbor/IMRS/
-MONTH=202403
+MONTH=202409
 SCRIPT="$HOME/ithitools/src/imrsrsv.py"
 RUN_DATE=$(date '+%Y-%m-%d')
 RUN_LOG="$HOME/tmp/$RUN_DATE.$$.txt"
@@ -16,4 +16,4 @@ echo "Month: $MONTH"
 echo "Script: $SCRIPT"
 echo "Log: $RUN_LOG"
 
-python3.6 $SCRIPT $IMRS_CBOR $IPSTATS_DIR $MONTH $ITHITOOLS > $RUN_LOG
+python $SCRIPT $IMRS_CBOR $IPSTATS_DIR $MONTH $ITHITOOLS > $RUN_LOG
