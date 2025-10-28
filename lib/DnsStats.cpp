5aca5f6f8687186595f5192d9d9090698419b3fa
huitema <huitema@huitema.net>
2025-10-28 11:32:36 -0700

    Remove  "DUNLOP" TLD

--- lib/DnsStats.cpp
+++ lib/DnsStats.cpp
@@ -221,7 +221,7 @@ char const * RegisteredTldName[] = {
     "DENTAL", "DENTIST", "DESI", "DESIGN", "DEV", "DHL", "DIAMONDS", "DIET", "DIGITAL",
     "DIRECT", "DIRECTORY", "DISCOUNT", "DISCOVER", "DISH", "DIY", "DJ", "DK", "DM", "DNP",
     "DO", "DOCS", "DOCTOR", "DOG", "DOMAINS", "DOT", "DOWNLOAD", "DRIVE",
-    "DTV", "DUBAI", "DUNLOP", "DUPONT", "DURBAN", "DVAG", "DVR", "DZ",
+    "DTV", "DUBAI", "DUPONT", "DURBAN", "DVAG", "DVR", "DZ",
     "EARTH", "EAT", "EC", "ECO", "EDEKA", "EDU", "EDUCATION", "EE", "EG", "EMAIL", "EMERCK",
     "ENERGY", "ENGINEER", "ENGINEERING", "ENTERPRISES", "EPSON", "EQUIPMENT", "ER",
     "ERICSSON", "ERNI", "ES", "ESQ", "ESTATE", "ET", "EU",
