64b6fb22d5ae00d5ec6d09bf68d3b58bc5f46d5e
huitema <huitema@huitema.net>
2026-04-29 22:17:09 -0700

    Parallel version of delta_t script

--- resolver/rsv_delta_t.py
+++ resolver/rsv_delta_t.py
@@ -21,6 +21,7 @@ from rsv_delay_class import delay_query_as
 import rsv_arguments
 import open_rsv
 import time
+import concurrent.futures
 
 
 delta_first_max = 0
@@ -159,7 +160,7 @@ class delta_t_rr_slice:
             self.uids[uid].check(query_time)
 
     def get_headers():
-        r = [ "nb_uids", "nb_uids_early", "nb_total", "nb_early",
+        r = [ "nb_uids", "uids_early", "nb_total", "nb_early",
               "max_delta", "sum_delta", "average_delta" ]
         for dn in delta_names:
             r.append(dn)
@@ -238,6 +239,7 @@ class delta_t_parse:
                             lth *= 2
         self.summarize()
         self.current_first_time = 0
+        return nb_events
 
     def get_df(self):
         self.summarize()
@@ -252,31 +254,177 @@ class delta_t_parse:
         df = pd.DataFrame(t, columns=headers)
         return df
 
+    def save_and_close(self, output_file):
+        df = self.get_df()
+        df.to_csv(output_file)
+
+class delta_t_rr_summary:
+    def __init__(self):
+        self.nb_uids = 0
+        self.uids_early = 0
+        self.nb_early = 0
+        self.nb_total = 0
+        self.max_delta = 0
+        self.sum_delta = 0
+        self.average_delta = 0
+        self.delta_set = [ 0, 0, 0, 0, 0, 0, 0, 0 ]
+
+    def add_row(self, x):
+        self.nb_uids += x['nb_uids']
+        self.uids_early += x['uids_early']
+        self.nb_early += x['nb_early']
+        self.nb_total += x['nb_total']
+        if x['max_delta'] > self.max_delta:
+            self.max_delta = x['max_delta']
+        self.sum_delta += x['sum_delta']
+        for i in range(0, len(delta_names)):
+            self.delta_set[i] += x[delta_names[i]]
+        if self.nb_uids > 0:
+            self.average_delta = self.sum_delta / self.nb_uids
+
+    def get_row(self):
+        r = [ self.nb_uids, self.uids_early, self.nb_total,
+            self.nb_early, self.max_delta, self.sum_delta, self.average_delta ]
+        for i in range(0, len(delta_set)):
+            r.append(self.delta_set[i])
+        return r
+
+class delta_t_summary:
+    def __init__(self):
+        self.rr_deltas = [ delta_t_rr_summary(), delta_t_rr_summary(), delta_t_rr_summary() ]
+        self.current_first_time = 0
+
+    def add_row(self, x):
+        i_rr = 2
+        if x["query_RR"] == "A":
+            i_rr = 0
+        elif x["query_RR"] == "AAAA":
+            i_rr = 1
+        self.rr_deltas[i_rr].add_row(x)
+        
+    def load(self, fn):
+        df = pd.read_csv(fn)
+        df.apply(lambda row: self.add_row(row),axis=1)
+
+    def get_df(self):
+        headers = [ 'query_RR' ] + delta_t_rr_slice.get_headers()
+
+        t = []
+        for i in range(0,3):
+            r = [ rr_names[i] ] + self.rr_deltas[i].get_row()
+            t.append(r)
+
+        df = pd.DataFrame(t, columns=headers)
+        return df
+
+#parallel
+
+
+class file_bucket:
+    def __init__(self, ip2a4, ip2a6, as_names, output_file, source_file, bucket_id, time_start):
+        self.ip2a4 = ip2a4
+        self.ip2a6 = ip2a6
+        self.as_names = as_names
+        self.output_file = output_file
+        self.source_file = source_file
+        self.bucket_id = bucket_id
+        self.time_start = time_start
+
+    def load(self):
+        if not os.path.exists(self.output_file):
+            dtp = delta_t_parse(self.ip2a4, self.ip2a6, self.as_names)
+            nb_events = dtp.load_log(self.source_file, time_start=self.time_start)
+            print(self.source_file + ": loaded " + str(nb_events) + " events at " + str(time.time() - self.time_start))
+            sys.stdout.flush()
+            dtp.save_and_close(self.output_file)
+        else:
+            print(self.output_file + ": already exists.")
+            sys.stdout.flush()
+
+def load_bucket(bucket):
+    bucket.load()
+    return True
+
 
 # main
+if __name__ == "__main__":
+    time_start = time.time()
+    if len(sys.argv) < 4:
+        usage()
+        exit(-1)
 
-# load the IP mapping tables
-source_path = Path(__file__).resolve()
-resolver_dir = source_path.parent
-auto_source_dir = resolver_dir.parent
-print("Auto source path is: " + str(auto_source_dir) + " (source: " + str(source_path) + ")")
-source_dir = os.path.join(auto_source_dir, "data")
-ip2a4_file = os.path.join(source_dir, "ip2as.csv")
-ip2a6_file = os.path.join(source_dir, "ip2asv6.csv")
-as_names_file = os.path.join(source_dir, "as_names.csv")
-ip2a4 = ip2as.ip2as_table()
-ip2a4.load(ip2a4_file)
-ip2a6 = ip2as.ip2as_table()
-ip2a6.load(ip2a6_file)
-as_names = ip2as.asname()
-as_names.load(as_names_file)
-time_loaded = time.time()
+    output_dir = sys.argv[1]
+    if not os.path.isdir(output_dir):
+        print("Invalid output folder.")
+        usage()
+        exit(-1)
 
-# load all the files
-dtp = delta_t_parse(ip2a4, ip2a6, as_names)
+    temp_dir = sys.argv[2]
+    if not os.path.isdir(temp_dir):
+        print("Invalid temporary folder.")
+        usage()
+        exit(-1)
 
-for file_path in sys.argv[2:]:
-    dtp.load_log(file_path, log_threshold=15625, time_start=0)
+    source_files, has_error = rsv_arguments.parse_file_list(sys.argv[3:], [ ".bz2", ".log", ".txt"])
+    if has_error:
+        print("Invalid list of input files.")
+        usage()
+        exit(-1)
+    # load the IP mapping tables
+    source_path = Path(__file__).resolve()
+    resolver_dir = source_path.parent
+    auto_source_dir = resolver_dir.parent
+    print("Auto source path is: " + str(auto_source_dir) + " (source: " + str(source_path) + ")")
+    source_dir = os.path.join(auto_source_dir, "data")
+    ip2a4_file = os.path.join(source_dir, "ip2as.csv")
+    ip2a6_file = os.path.join(source_dir, "ip2asv6.csv")
+    as_names_file = os.path.join(source_dir, "as_names.csv")
+    ip2a4 = ip2as.ip2as_table()
+    ip2a4.load(ip2a4_file)
+    ip2a6 = ip2as.ip2as_table()
+    ip2a6.load(ip2a6_file)
+    as_names = ip2as.asname()
+    as_names.load(as_names_file)
+    time_loaded = time.time()
 
-df = dtp.get_df()
-df.to_csv(sys.argv[1])
+    # prepare the buckets
+    print("Tables loaded at " + str(time_loaded - time_start) + " seconds.")
+
+    # Prepare the parallel buckets
+    nb_process = len(source_files)
+    bucket_list = []
+    bucket_id = 0
+    for source_file in source_files:
+        item = os.path.basename(source_file)
+        item = item[:-4]
+        if item.startswith("queries"):
+            item = item[7:]
+        output_file = os.path.join(temp_dir, "delta_t-" + item + ".csv")
+        bucket = file_bucket(ip2a4, ip2a6, as_names, output_file, source_file, bucket_id, time_loaded)
+        bucket_list.append(bucket)
+        bucket_id += 1
+
+    nb_process = len(bucket_list)
+    print("Will use " + str(nb_process) + " processes.")
+
+    with concurrent.futures.ProcessPoolExecutor(max_workers = nb_process) as executor:
+        future_to_bucket = {executor.submit(load_bucket, bucket):bucket for bucket in bucket_list }
+        for future in concurrent.futures.as_completed(future_to_bucket):
+            bucket = future_to_bucket[future]
+            try:
+                data = future.result()
+                print('Bucket %d complete' % (bucket.bucket_id))
+            except Exception as exc:
+                traceback.print_exc()
+                print('Bucket %d generated an exception: %s' % (bucket.bucket_id, exc))
+    bucket_time = time.time()
+
+    # Summarize all the results 
+    
+    delta_t_total = delta_t_summary()
+    for bucket in bucket_list:
+        delta_t_total.load(bucket.output_file)
+
+    delta_t_file = os.path.join(output_dir, "delta_t.csv")
+    df = delta_t_total.get_df()
+    df.to_csv(delta_t_file)
